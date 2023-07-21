f3b19ace66e2d6872e0e8ae9adff2dbd61b5936d
PipZip <37911683+PipZip@users.noreply.github.com>
2023-07-21 18:31:12 +0600

    init

---
+++ gulp-tasks/scripts.js
@@ -0,0 +1,30 @@
+"use strict";
+
+import { paths } from "../gulpfile.babel";
+import gulp from "gulp";
+import gulpif from "gulp-if";
+import rename from "gulp-rename";
+import browsersync from "browser-sync";
+import debug from "gulp-debug";
+import yargs from "yargs";
+import fileinclude from "gulp-file-include";
+
+const argv = yargs.argv,
+    production = !!argv.production;
+
+
+gulp.task("scripts", () => {
+    return gulp.src(paths.scripts.src)
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulpif(production, rename({
+            suffix: ".min"
+        })))
+        .pipe(gulp.dest(paths.scripts.dist))
+        .pipe(debug({
+            "title": "JS files"
+        }))
+        .pipe(browsersync.stream());
+});
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+//libs
+@@include("./libs/jquery-3.7.0.min.js")
+
+//my files
+@@include("./import/main.js")
