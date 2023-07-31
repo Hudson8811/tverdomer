29190fc76bc21e7c555c6bcf7d2d1c9f83be974e
kihhmaster <kihhmaster@yandex.ru>
2023-07-31 18:54:33 +0300

    Порезка на компоненты

--- src/js/import/main.js
+++ src/js/import/main.js
@@ -1,5 +1,30 @@
 jQuery(function () { 
-  // Fancybox.bind("[data-fancybox]", {
-  //   // Your custom options
-  // });
+
+  new Skroll()
+    .add(".hardness__bg",{
+            animation:"fadeInRight",
+            delay: 600,
+            duration:750
+    })
+    .add(".warranty__bg",{
+            animation:"fadeInRight",
+            delay: 900,
+            duration:750
+    })
+    .add(".ultrasonic__bg",{
+            animation:"fadeInUp",
+            delay: 900,
+            duration:750
+    })
+    .add(".security__bg",{
+            animation:"fadeInUp",
+            delay: 900,
+            duration:750
+    })
+    .add(".measure__bg",{
+            animation:"fadeInLeft",
+            delay: 900,
+            duration:750
+    })
+    .init();
 });
