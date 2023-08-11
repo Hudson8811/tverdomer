1df2df487d34aff8d1a899926a476fd4cf3a7806
kihhmaster <kihhmaster@yandex.ru>
2023-08-11 15:00:06 +0300

    Корзина и каталог 2 страницы

--- dist/js/main.js
+++ dist/js/main.js
@@ -360,3 +360,4 @@ accordions.forEach((accordion) => {
 
 
 
+
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,4 @@
 @@include("../../blocks/modules/main-slider/main-slider.js")
 @@include("../../blocks/modules/products/products.js")
 @@include("../../blocks/modules/header/header.js")
+
