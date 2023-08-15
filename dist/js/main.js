d371497f19db9c131a7859b22e0c61cfac340fa8
KostaSenatov <kostyasenatov@gmail.com>
2023-08-15 20:17:17 +0400

    .

--- dist/js/main.js
+++ dist/js/main.js
@@ -437,7 +437,7 @@ galleryMain.controller.control =  galleryThumbs;
 
 //show-more
 	jQuery(document).ready(function($){
-		$('.news__select').select2();
+		$('.myselect-class').select2();
 
 
 		$(".news-show-more-mybtn1").click(function(e){
@@ -459,3 +459,53 @@ galleryMain.controller.control =  galleryThumbs;
 	})
 
 
+var Swiper = new Swiper(".new__slider .swiper", {
+	slidesPerView: 1.2,
+	watchOverflow: true,
+	navigation: {
+		nextEl: '.new-swiper-button-next',
+		prevEl: '.new-swiper-button-prev',
+	},
+	breakpoints: {
+		768: {
+			slidesPerView: 2,
+		}
+	}
+});
+
+
+if (typeof ymaps !== "undefined") {
+	ymaps.ready(() => {
+		initYmaps("hero-contacts__map", [55.57136017671344, 37.75413504365536]);
+	});
+}
+function initYmaps(mapID, coord) {
+	const map = new ymaps.Map(mapID, {
+		center: coord,
+		zoom: 16
+	});
+	const myPlacemark = new ymaps.Placemark(
+		coord,
+		{
+			// hintContent: 'Подсказка при наведении на метку',
+			// balloonContent: 'Текст при нажатии на метку'
+		},
+		{
+			iconLayout: "default#image",
+			iconImageHref: "",
+			iconImageSize: [44, 56],
+			iconImageOffset: [-22, -56]
+		}
+	);
+
+	map.geoObjects.add(myPlacemark);
+
+   map.controls.remove('geolocationControl'); // удаляем геолокацию
+   map.controls.remove('searchControl'); // удаляем поиск
+   map.controls.remove('trafficControl'); // удаляем контроль трафика
+   map.controls.remove('typeSelector'); // удаляем тип
+   map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+   map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+   map.controls.remove('rulerControl'); // удаляем контрол правил
+  // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
