daac54729afaffb2e1473b00f975cc518c7b96be
dmitryboz21 <dmitryboz21@yandex.com>
2023-08-25 15:55:36 +0300

    123

--- src/js/import/maps.js
+++ src/js/import/maps.js
@@ -515,6 +515,25 @@ function getYaMap() {
 						/*	filterCenters(myMap1, clusterer, $('#localities-centers__select').val(), $('.localities-centers__button-active.js-map-filter-brand').attr('data-filter'));*/
 						});
 
+
+
+						var isMobile = {
+							Android: function() {return navigator.userAgent.match(/Android/i);},
+							BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+							iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+							Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+							Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+							any: function() {
+								return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+							}
+						};
+						
+						// после вызова карты
+						if(isMobile.any()){
+							//myMap1.behaviors.disable('scrollZoom');
+							myMap1.behaviors.disable('drag');
+						}
+
 					}
 
 
