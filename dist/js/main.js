11690358357ef7ce80d949ca887c36971d1ddd50
kihhmaster <kihhmaster@yandex.ru>
2023-09-05 18:46:51 +0300

    правки,product-card__slider, личный кабинет

--- dist/js/main.js
+++ dist/js/main.js
@@ -5071,54 +5071,118 @@ humb.addEventListener("click", function () {
 
 
 
-var galleryThumbs = new Swiper(".gallery-thumbs", {
-  centeredSlides: true,
-  centeredSlidesBounds: true,
-  slidesPerView: 3,
-  watchOverflow: true,
-  watchSlidesVisibility: true,
-  watchSlidesProgress: true,
-  direction: 'horizontal',
-  spaceBetween: 10,
-  navigation: {
-          nextEl: ".gallery-thumbs-next",
-          prevEl: ".gallery-thumbs-prev"
-  },
-    breakpoints: {
-  	// when window width is >= 480px
-  	992: {
-  		direction: 'vertical',
-      spaceBetween: 14,
-  	},
-  }
+
+
+// var galleryThumbs = new Swiper(".gallery-thumbs", {
+//   centeredSlides: true,
+//   centeredSlidesBounds: true,
+//   slidesPerView: 3,
+//   watchOverflow: true,
+//   watchSlidesVisibility: true,
+//   watchSlidesProgress: true,
+//   direction: 'horizontal',
+//   spaceBetween: 10,
+//   navigation: {
+//           nextEl: ".gallery-thumbs-next",
+//           prevEl: ".gallery-thumbs-prev"
+//   },
+//     breakpoints: {
+//   	// when window width is >= 480px
+//   	992: {
+//   		direction: 'vertical',
+//       spaceBetween: 14,
+//   	},
+//   }
   
+// });
+
+// var galleryMain = new Swiper(".gallery-main",  {
+//   watchOverflow: true,
+//   watchSlidesVisibility: true,
+//   watchSlidesProgress: true,
+//   preventInteractionOnTransition: true,
+//   slidesPerView: 1,
+
+//   thumbs: {
+//           swiper: galleryThumbs
+//   },
+//   navigation: {
+//     nextEl: ".gallery-main-next",
+//     prevEl: ".gallery-main-prev"
+// },
+//   // breakpoints: {
+//   // 	// when window width is >= 480px
+//   // 	992: {
+//   // 		slidesPerView: 2.2,
+//   // 		spaceBetween: 30
+//   // 	},
+//   // }
+// });
+
+// // galleryThumbs.controller.control =  galleryMain;
+// galleryMain.controller.control =  galleryThumbs;
+
+
+
+$('.product-card__slider').each(function () {
+  var galleryThumbs;
+  var galleryMain;
+  var group = $(this);
+  group.find('.gallery-thumbs').each(function () {
+      var slider = $(this)
+      galleryThumbs = new Swiper(slider[0], {
+          centeredSlides: true,
+          centeredSlidesBounds: true,
+          slidesPerView: 3,
+          watchOverflow: true,
+          watchSlidesVisibility: true,
+          watchSlidesProgress: true,
+          direction: 'horizontal',
+          spaceBetween: 10,
+          navigation: {
+              nextEl: ".gallery-thumbs-next",
+              prevEl: ".gallery-thumbs-prev"
+          },
+          breakpoints: {
+              // when window width is >= 480px
+              992: {
+                  direction: 'vertical',
+                  spaceBetween: 14,
+              },
+          }
+      })
+  });
+
+  group.find('.gallery-main').each(function () {
+      var slider = $(this)
+      galleryMain = new Swiper(slider[0], {
+          watchOverflow: true,
+          watchSlidesVisibility: true,
+          watchSlidesProgress: true,
+          preventInteractionOnTransition: true,
+          slidesPerView: 1,
+
+          thumbs: {
+              swiper: galleryThumbs
+          },
+          navigation: {
+              nextEl: ".gallery-main-next",
+              prevEl: ".gallery-main-prev"
+          },
+          // breakpoints: {
+          //     // when window width is >= 480px
+          //     992: {
+          //         slidesPerView: 2.2,
+          //         spaceBetween: 30
+          //     },
+          // }
+      })
+      // galleryMain.controller.control = galleryThumbs;
+  });
+
+  // // galleryThumbs.controller.control = galleryMain;
+  galleryMain.controller.control = galleryThumbs;
 });
-
-var galleryMain = new Swiper(".gallery-main",  {
-  watchOverflow: true,
-  watchSlidesVisibility: true,
-  watchSlidesProgress: true,
-  preventInteractionOnTransition: true,
-  slidesPerView: 1,
-
-  thumbs: {
-          swiper: galleryThumbs
-  },
-  navigation: {
-    nextEl: ".gallery-main-next",
-    prevEl: ".gallery-main-prev"
-},
-  // breakpoints: {
-  // 	// when window width is >= 480px
-  // 	992: {
-  // 		slidesPerView: 2.2,
-  // 		spaceBetween: 30
-  // 	},
-  // }
-});
-
-// galleryThumbs.controller.control =  galleryMain;
-galleryMain.controller.control =  galleryThumbs;
 
 // $(".main__pagination__item").click(function(event){     
 //     event.preventDefault();
