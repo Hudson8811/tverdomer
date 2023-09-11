3e85ab9c7fca94a7d6848138248f8ad43b10a938
kihhmaster <kihhmaster@yandex.ru>
2023-09-11 15:51:08 +0300

    Правки main-menu, main-slider, таблица

--- src/blocks/modules/main-pagination/main-pagination.js
+++ src/blocks/modules/main-pagination/main-pagination.js
@@ -5,17 +5,17 @@
 //     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 // });
 
-$(document).ready(function () {
-	$(function () {
-			$('.main__pagination__item').click(function (e) {
-					$('.main__pagination__item').removeClass('active');
-					$(this).addClass('active');
-			});
-	});
-});
+// $(document).ready(function () {
+// 	$(function () {
+// 			$('.main__pagination__item').click(function (e) {
+// 					$('.main__pagination__item').removeClass('active');
+// 					$(this).addClass('active');
+// 			});
+// 	});
+// });
 window.addEventListener('scroll', event => {
   let navigationLinks = document.querySelectorAll('.main__pagination__item');
-  let fromTop = window.scrollY;
+  let fromTop = window.scrollY + 72;
   navigationLinks.forEach(link => {
     let section = document.querySelector(link.hash);
     if (
