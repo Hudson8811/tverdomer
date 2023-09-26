4430178396dc7afabbbf39ecd74e88b14573ee5b
PipZip <37911683+PipZip@users.noreply.github.com>
2023-09-26 15:55:11 +0600

    remove tabs

--- dist/js/main.js
+++ dist/js/main.js
@@ -5136,38 +5136,6 @@ $('.js-main__slider').each(function(){
 	});
 })
 
-	///tabs
-	
-	var ProductsTabNavs = document.querySelectorAll(".products__tab");
-	var ProductsTabPanes = document.querySelectorAll(".products-pane");
-  if(ProductsTabNavs !==null & ProductsTabPanes !==null) {
-		tabClick() 
-	}
-	function tabClick() {
-		console.log("клик")
-		for (var i = 0; i < ProductsTabNavs.length; i++) {
-
-			ProductsTabNavs[i].addEventListener("click", function(e){
-				e.preventDefault();
-				var activeTabAttr = e.target.getAttribute("data-tab");
-	
-				for (var j = 0; j < ProductsTabNavs.length; j++) {
-					var contentAttr = ProductsTabPanes[j].getAttribute("data-tab-content");
-	
-					if (activeTabAttr === contentAttr) {
-						ProductsTabNavs[j].classList.add("active");
-						ProductsTabPanes[j].classList.add("active"); 
-					} else {
-						ProductsTabNavs[j].classList.remove("active");
-						ProductsTabPanes[j].classList.remove("active");
-					}
-				};
-			});
-		}
-	}
-	///tabs
-
-
 
 $( ".header__menu__item" ).mouseenter(function () {
   let headerMenuItem = $(this);
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,5 +1,4 @@
 @@include("../../blocks/modules/main-slider/main-slider.js")
-@@include("../../blocks/modules/products/products.js")
 @@include("../../blocks/modules/header/header.js")
 @@include("../../blocks/modules/product-card-slider/product-card-slider.js")
 @@include("../../blocks/modules/main-pagination/main-pagination.js")
