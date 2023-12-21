38fb6f8418e115e15817b13bf183de0e13285f89
unknown <dmitryboz21@yandex.com>
2023-12-21 09:10:13 +0300

    fixed-animations-on-landing

--- dist/js/import/main.js
+++ dist/js/import/main.js
@@ -32,8 +32,8 @@ jQuery(function () {
 	new Skroll()
 		.add(".portable__picture", {
 			animation: "fadeInRight",
-			delay: 200,
-			duration: 750
+			delay: 500,
+			duration: 750,
 		})
 		.add(".portable__col__one", {
 			animation: "fadeInLeft",
@@ -95,7 +95,7 @@ if (document.documentElement.clientWidth < 767) {
 		allowHTML: true,
 		// placement: 'right',
 		inlinePositioning: true,
-	
+
 	});
 } else {
 	tippy('.hint', {
@@ -110,7 +110,7 @@ if (document.documentElement.clientWidth < 767) {
 		zIndex:	900,
 		placement: 'right',
 		inlinePositioning: true,
-	
+
 	});
 };
 
@@ -229,9 +229,9 @@ if(btnCity !==null) {
 					cities.classList.add("animate__fadeOutRight", "noActive")
 					item.innerText = "списком";
 				}
-			})  
+			})
 		}
-	
+
 	})
 }
 
@@ -291,17 +291,17 @@ $('.product-cart__filter__counter__prev').on('click', function () {
 	var $input = $(this).siblings('input');
 	var count = parseInt($count.text(), 10);
 	var minimum = 0;
-    
+
 	if ($input.data('minimum')) {
 	  minimum = parseInt($input.data('minimum'), 10);
 	}
-    
+
 	count--;
-    
+
 	if (count < minimum) {
 	  count = minimum;
 	}
-    
+
 	$count.text(count);
 	$input.val(count);
 
@@ -330,7 +330,7 @@ $('.js-mask-phone').mask('+7(000)000-00-00');
 
 (function() {
         const fileLoaderContainers = document.querySelectorAll('.file-loader');
-      
+
         const renderFileLoader = function(fileLoaderContainer) {
           const wrapper = fileLoaderContainer.querySelector('.file-loader__wrapper');
           const btnAdd = fileLoaderContainer.querySelector('.js-fileloader-open');
@@ -339,20 +339,20 @@ $('.js-mask-phone').mask('+7(000)000-00-00');
           }
           const inputFileLoader = wrapper.querySelector('.file-loader__item');
           const inputTemplate = inputFileLoader.cloneNode(true);
-      
+
           const maxLoaders = 10;
           const limitBiteSize = 18000000;
-      
+
           const testImg = function(str) {
             return /\.(png|jpe?g|pdf|doc|docx)$/i.test(str);
           };
-      
+
           const setError = function(message) {
             if (btnAdd) {
               btnAdd.dataset.errorMess = message;
             }
           };
-      
+
           const isFileSizeCorrect = function(ctx) {
             const inputs = ctx.querySelectorAll('.file-loader-js');
             let size = 0;
@@ -363,7 +363,7 @@ $('.js-mask-phone').mask('+7(000)000-00-00');
             });
             return size < limitBiteSize;
           };
-      
+
           const renderNewInputAttr = function(attr) {
             const inputs = wrapper.querySelectorAll('.file-loader-js');
             const lastInput = inputs[inputs.length - 1];
@@ -373,7 +373,7 @@ $('.js-mask-phone').mask('+7(000)000-00-00');
             const currentName = lastInput[attr].match(regExpName)[0];
             return currentName + (currentNumber + 1);
           };
-      
+
           const addFileLoader = function() {
             const id = renderNewInputAttr('id');
             const name = renderNewInputAttr('name');
@@ -384,7 +384,7 @@ $('.js-mask-phone').mask('+7(000)000-00-00');
             wrapper.append(loaderItem);
             btnAdd.setAttribute('for', id );
           };
-      
+
           const formatSize = function(number) {
             let unitSize = ' Байт';
             let divider = 1;
@@ -397,7 +397,7 @@ $('.js-mask-phone').mask('+7(000)000-00-00');
             }
             return (number / divider).toFixed(1) + unitSize;
           };
-      
+
           wrapper.addEventListener('change', function(e) {
             const target = e.target;
             const parent = target.parentElement;
@@ -427,7 +427,7 @@ $('.js-mask-phone').mask('+7(000)000-00-00');
             setError(errorMess);
           });
         };
-      
+
         fileLoaderContainers.forEach(f => renderFileLoader(f));
 })();
 
@@ -454,11 +454,11 @@ const questionOpenAccordion = (accordion) => {
 			$('html').scrollTop($('html').scrollTop()+1);
 			setTimeout(() => {
 				sidebar.updateSticky();
-				
+
 			}, 150);
 		}, 150);
 	}, 250);
-	
+
 };
 
 const questionCloseAccordion = (accordion) => {
@@ -473,7 +473,7 @@ const questionCloseAccordion = (accordion) => {
 			$('html').scrollTop($('html').scrollTop()+1);
 			setTimeout(() => {
 				sidebar.updateSticky();
-				
+
 			}, 150);
 		}, 150);
 	}, 250);
@@ -522,9 +522,9 @@ if(companyCard != null) {
 				selectionCssClass: 'select2-named-example-form'
 			});
 		}
-	
+
 		initSelectsLikeAnketaPage();
-	
+
 		function yearsGenerator(startYear) {
 			var currentYear = new Date().getFullYear(), years = [];
 			startYear = startYear || 1980;
@@ -533,10 +533,10 @@ if(companyCard != null) {
 			}
 			return years.reverse();
 		}
-	
-	
+
+
 		function initDatePicker() {
-	
+
 			const datePickerInput = document.getElementById('date-picker');
 			// Initialize the date picker
 			if (!$(datePickerInput).hasClass('js-flag-init-dp')) {
@@ -551,59 +551,59 @@ if(companyCard != null) {
 					onOpen: [
 						function (selectedDates, dateStr, instance) {
 							var container = $(instance.calendarContainer);
-	
+
 							var cal_year = false;
 							var cal_month = false;
 							var cal_custom_year = false;
 							var cal_custom_month = false;
-	
-	
+
+
 							if (container.find('.datepicker-btns').length < 1) {
 								container.find('.flatpickr-innerContainer').after('<div class="datepicker-btns"><div class="datepicker-btn datepicker-btn--reject js-datepicker-btn-reject"><span>отмена</span></div><div class="datepicker-btn datepicker-btn--accept js-datepicker-btn-accept"><span>выбрать</span></div></div>');
-	
+
 								var years = yearsGenerator(1900);
 								var years_opt_list = '';
 								var currentYear = new Date().getFullYear()
 								years.forEach(year => {
 									if (year === currentYear) {
 										years_opt_list += '<option  value="' + year + '" selected>' + year + '</option>';
-	
+
 									}
 									else {
 										years_opt_list += '<option  value="' + year + '">' + year + '</option>';
 									}
 								});
-	
+
 								var months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-	
+
 								container.find('.flatpickr-months').before('<div class="flatpickr-custom-selects"><div class="flatpickr-custom-select flatpickr-custom-select--month">' +
 									'<select class="js-example-form">' + '<option class="flatpickr-monthDropdown-month" value="0" tabindex="-1">Январь</option><option class="flatpickr-monthDropdown-month" value="1" tabindex="-1">Февраль</option><option class="flatpickr-monthDropdown-month" value="2" tabindex="-1">Март</option><option class="flatpickr-monthDropdown-month" value="3" tabindex="-1">Апрель</option><option class="flatpickr-monthDropdown-month" value="4" tabindex="-1">Май</option><option class="flatpickr-monthDropdown-month" value="5" tabindex="-1">Июнь</option><option class="flatpickr-monthDropdown-month" value="6" tabindex="-1">Июль</option><option class="flatpickr-monthDropdown-month" value="7" tabindex="-1">Август</option><option class="flatpickr-monthDropdown-month" value="8" tabindex="-1">Сентябрь</option><option class="flatpickr-monthDropdown-month" value="9" tabindex="-1">Октябрь</option><option class="flatpickr-monthDropdown-month" value="10" tabindex="-1">Ноябрь</option><option class="flatpickr-monthDropdown-month" value="11" tabindex="-1">Декабрь</option>' + '</select><div class="simple-select2-items-wrapper"></div>' +
 									'</div><div class="flatpickr-custom-select flatpickr-custom-select--year">' +
 									'<select class="js-example-form">' + years_opt_list + '</select><div class="simple-select2-items-wrapper"></div>' +
 									'</div></div>');
-	
-	
+
+
 								dropdownParent: $(this).siblings('.simple-select2-items-wrapper')
-	
+
 								var cal_year = container.find('.numInput.cur-year');
 								var cal_month = container.find('.flatpickr-monthDropdown-months');
 								var cal_custom_year = container.find('.flatpickr-custom-select--year select');
 								var cal_custom_month = container.find('.flatpickr-custom-select--month select');
-	
+
 								cal_custom_year.on('change', function () {
 									console.log($(this).val());
 									//cal_year.val($(this).val());
-	
+
 									flatpickrItem.jumpToDate(new Date(cal_custom_year.val(), cal_custom_month.val(), 1));
-	
+
 								});
 								cal_custom_month.on('change', function () {
 									console.log($(this).val());
 									cal_month.val($(this).val());
 									flatpickrItem.changeMonth(parseInt($(this).val()), false);
 								});
-	
-	
+
+
 								container.find('.js-example-form').each(function (index) {
 									var select = $(this);
 									select.select2({
@@ -615,21 +615,21 @@ if(companyCard != null) {
 										dropdownParent: $(this).siblings('.simple-select2-items-wrapper')
 									});
 								});
-	
-	
+
+
 								//'<select class="js-example-form"><option  value="" selected>Экология и рациональное природопользование</option></select>'
 							}
-	
-	
+
+
 							cal_custom_year.val(cal_year.val()).trigger('change')
 							cal_custom_month.val(cal_month.val()).trigger('change')
-	
+
 						}
 					],
 				});
 				$(datePickerInput).addClass('js-flag-init-dp');
-	
-	
+
+
 				$('body').on('click', '.js-datepicker-btn-reject, .js-datepicker-btn-accept', function (e) {
 					e.preventDefault();
 					e.stopPropagation();
@@ -638,17 +638,17 @@ if(companyCard != null) {
 			}
 		}
 		initDatePicker();
-	
+
 		// $('body').on('click', '.js-datepicker-btn-reject, .js-datepicker-btn-accept', function () {
 		// 	flatpickrItem.close();
 		// });
-	
+
 		$('body').on('reinitAnketaFormCompomnents', function () {
 			initSelectsLikeAnketaPage();
 			initDatePicker();
-	
+
 		});
-	
+
 	});
 }
 
